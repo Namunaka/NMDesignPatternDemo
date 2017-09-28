604154829339f29e9d3c3d983141b885765ed3ae
大黄 <namunaka@gmail.com>
2017-09-28 15:38:04 +0800

    基本概念

---
+++ NMDesignPatternDemo/NMDesignPatternDemo/BuilderViewController.h
@@ -0,0 +1,13 @@
+//
+//  BuilderViewController.h
+//  NMDesignPatternDemo
+//
+//  Created by 纳木那咔 on 2017/9/12.
+//  Copyright © 2017年 纳木那咔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BuilderViewController : UIViewController
+
+@end
---
+++ NMDesignPatternDemo/NMDesignPatternDemo/BuilderViewController.m
@@ -0,0 +1,37 @@
+//
+//  BuilderViewController.m
+//  NMDesignPatternDemo
+//
+//  Created by 纳木那咔 on 2017/9/12.
+//  Copyright © 2017年 纳木那咔. All rights reserved.
+//
+
+#import "BuilderViewController.h"
+
+@interface BuilderViewController ()
+
+@end
+
+@implementation BuilderViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
