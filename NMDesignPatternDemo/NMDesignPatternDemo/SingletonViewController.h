75ab8b1d77916a884de2c3e828bf7eb2a8bf10a7
大黄 <namunaka@gmail.com>
2017-09-30 14:28:05 +0800

    适配器

---
+++ NMDesignPatternDemo/NMDesignPatternDemo/SingletonViewController.h
@@ -0,0 +1,13 @@
+//
+//  SingletonViewController.h
+//  NMDesignPatternDemo
+//
+//  Created by 纳木那咔 on 2017/9/29.
+//  Copyright © 2017年 纳木那咔. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SingletonViewController : UIViewController
+
+@end
---
+++ NMDesignPatternDemo/NMDesignPatternDemo/适配器(Adaptee)/AdapteeViewController.m
@@ -0,0 +1,37 @@
+//
+//  AdapteeViewController.m
+//  NMDesignPatternDemo
+//
+//  Created by 纳木那咔 on 2017/9/29.
+//  Copyright © 2017年 纳木那咔. All rights reserved.
+//
+
+#import "AdapteeViewController.h"
+
+@interface AdapteeViewController ()
+
+@end
+
+@implementation AdapteeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
